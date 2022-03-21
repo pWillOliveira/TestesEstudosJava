ad35902f7ffe10ceba655b883f1d552b182001b4
pWillOliveira <wpo_0072@hotmail.com>
2022-03-21 17:32:08 -0300

    Adicionado alguns operadores

--- src/main/java/com/mycompany/testetipos/Operadores.java
+++ src/main/java/com/mycompany/testetipos/Operadores.java
@@ -3,10 +3,26 @@ package com.mycompany.testetipos;
 public class Operadores {
     public static void main(String[] args) {
         //Operadores Aritméticos
-        int n1 = 3;
+        /*int n1 = 3;
         int n2 = 5;
         float m = (n1 + n2)/2;
-        System.out.println("A média é igual a: " + m);
+        System.out.println("A média é igual a: " + m); 
+
+        int numero = 5;
+        numero++;
+        System.out.println(numero);
+        
+        /*int x = 4;
+        int v = (int) Math.pow(2, 2); //Potenciação
+        x += v;
+        System.out.println(x);
+        
+        int n1, n2 ,r; //Operador lógico ternário
+        n1 = 4;
+        n2 = 8;
+        r = (n1>n2)?0:1;
+        System.out.println(r); */
+        
     }
     
 }
